a15545aeb855f79ac82b817fe144e6f935cae784
JooMin Choi <joominchoi23@gmail.com>
2023-08-09 15:18:12 +0100

    Update names of classes

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const container = document.querySelector(".container");
+const container = document.querySelector(".grid");
 
 function makeGrid() {
 	for (i = 1; i <= 16; i++) {
 		for (j = 1; j <= 16; j++) {
 			const row = document.createElement("div");
-			row.classList.add("grid")
+			row.classList.add("square")
 			container.appendChild(row);
 		}
 	}
@@ -12,9 +12,9 @@ function makeGrid() {
 
 makeGrid();
 
-const grid = document.getElementsByClassName("grid")
+const squares = document.getElementsByClassName("square")
 
-for (let square of grid) {
+for (let square of squares) {
 	square.addEventListener(
 				"mouseover",
 				(event) => {
