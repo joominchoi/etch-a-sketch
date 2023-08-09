c83a7fcdfca7c2db65ea08017ede3525cd37e068
JooMin Choi <joominchoi23@gmail.com>
2023-08-09 15:05:05 +0100

    Add a class to the created div and a function that iterates through divs
    - This function is required so that hover effect only applies to the
    squares of the grid and no where else

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ function makeGrid() {
 	for (i = 1; i <= 16; i++) {
 		for (j = 1; j <= 16; j++) {
 			const row = document.createElement("div");
+			row.classList.add("grid")
 			container.appendChild(row);
 		}
 	}
@@ -11,9 +12,13 @@ function makeGrid() {
 
 makeGrid();
 
-container.addEventListener(
-  "mouseover",
-  (event) => {
-    event.target.style.background = "orange";
-	}
-)
+const grid = document.getElementsByClassName("grid")
+
+for (let square of grid) {
+	square.addEventListener(
+				"mouseover",
+				(event) => {
+					event.target.style.background = "orange";
+				}
+			)
+}
