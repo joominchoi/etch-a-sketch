fe05c6e2685f5c9ecd6f4da8ed46e5e1505c8187
JooMin Choi <joominchoi23@gmail.com>
2023-08-10 17:36:23 +0100

    Update grid size according to user input
    - Created removeGrid function

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const gridSizeButton = document.getElementById("gridSizeButton")
 const gridStyle = window.getComputedStyle(grid)
 const gridHeight = Number(gridStyle.getPropertyValue('height').replace('px', ''));
 
-numberPerSide = 75
-squareSize = (gridHeight - (numberPerSide * 2)) / numberPerSide
+let numberPerSide = 5
+let squareSize = (gridHeight - (numberPerSide * 2)) / numberPerSide
 
 function makeGrid() {
 	for (i = 1; i <= numberPerSide; i++) {
@@ -14,7 +14,6 @@ function makeGrid() {
 			const row = document.createElement("div");
 			row.classList.add("square");
 			grid.appendChild(row);
-
 		}
 	}
 }
@@ -23,11 +22,17 @@ function updateGridSize() {
 	grid.style.gridTemplateColumns = `repeat(${numberPerSide}, auto)`;
 }
 
-function updateSquareSize(){
-  Array.from(squares).forEach(function(square) {
-      square.style.height = `${squareSize}px`;
-      square.style.width = `${squareSize}px`;
-  });
+function updateSquareSize() {
+	Array.from(squares).forEach(function (square) {
+		square.style.height = `${squareSize}px`;
+		square.style.width = `${squareSize}px`;
+	});
+}
+
+function removeGrid() {
+	while(squares.length > 0) {
+		squares[0].remove();
+ }
 }
 
 makeGrid();
@@ -43,18 +48,33 @@ for (let square of squares) {
 	)
 }
 
+// Apply the addEventListener method
+gridSizeButton.addEventListener("click", myFunc)
 
-      // let counter = document.getElementById("counter")
-      
-      // Apply the addEventListener method
-      gridSizeButton.addEventListener("click", myFunc)
-      
-      // Defining the myFunc function
-      function myFunc() {
-      
-         // Increase the existing value by 1      
-         // Use the parseInt method to convert the existing      
-         // value (which is in string format) into integer
-         console.log("Hello")
-      }
+// Defining the myFunc function
+function myFunc() {
 
+	// Increase the existing value by 1      
+	// Use the parseInt method to convert the existing      
+	// value (which is in string format) into integer
+	console.log("Hello")
+
+	numberPerSide = prompt("Grid size: Please enter a number between 0-100", `${numberPerSide}`);
+	squareSize = (gridHeight - (numberPerSide * 2)) / numberPerSide
+	
+	if ((numberPerSide > 0) && (numberPerSide <= 100)) {
+		removeGrid();
+		makeGrid();	
+		updateGridSize();
+		updateSquareSize();
+	} else {
+		alert("Invalid number");
+	}
+
+	console.log(numberPerSide)
+	console.log(squareSize)
+	// if (person != null) {
+	// 	document.getElementById("demo").innerHTML =
+	// 		"Hello " + person + "! How are you today?";
+	// }
+}
