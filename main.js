982d90d2c38e86b4990aea7c0ae955077dc269f3
JooMin Choi <joominchoi23@gmail.com>
2023-08-10 15:27:17 +0100

    Add function that updates width and height of squares dynamically

--- main.js
+++ main.js
@@ -1,29 +1,38 @@
-const container = document.querySelector(".grid");
+const grid = document.querySelector(".grid");
 
-numberPerSide = 5
+numberPerSide = 100
 
-container.style.gridTemplateColumns = `repeat(${numberPerSide}, auto)`;
+grid.style.gridTemplateColumns = `repeat(${numberPerSide}, auto)`;
 
 function makeGrid() {
 	for (i = 1; i <= numberPerSide; i++) {
 		for (j = 1; j <= numberPerSide; j++) {
 			const row = document.createElement("div");
 			row.classList.add("square");
-			container.appendChild(row);
-			
+			grid.appendChild(row);
+
 		}
 	}
 }
 
+function makeSquare(){
+  var elements = document.getElementsByClassName('square');
+  Array.from(elements).forEach(function(element) {
+      element.style.height = '1px';
+      element.style.width = '1px';
+  });
+}
+
 makeGrid();
+makeSquare();
 
 const squares = document.getElementsByClassName("square")
 
 for (let square of squares) {
 	square.addEventListener(
-				"mouseover",
-				(event) => {
-					event.target.style.background = "orange";
-				}
-			)
+		"mouseover",
+		(event) => {
+			event.target.style.background = "orange";
+		}
+	)
 }
